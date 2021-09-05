d5d2f8b2f4b7f346885218688a92192d97e86e0f
Hui-Erh Chai <89216763+Hui-Erh@users.noreply.github.com>
2021-09-05 15:35:38 -0500

    Update KNN and decision tree classifier_Hui-Erh Chai.py

--- KNN and decision tree classifier_Hui-Erh Chai.py
+++ KNN and decision tree classifier_Hui-Erh Chai.py
@@ -8,28 +8,28 @@ Created on Thu Sep  2 09:03:10 2021
 import pandas as pd
 df = pd.read_csv('Treasury Squeeze raw score data.csv', sep = ',')
 
-# mapping
-#df = df.map({'True':0, 'False':1})
+# converting type of columns to 'category'
+df['squeeze'] = df['squeeze'].astype('category')
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+df['squeeze'] = le.fit_transform(df['squeeze'])
+print(df)
 
 # Identify X and y
 X = df.iloc[:, 2:-1].values
 y = df.iloc[:, -1].values
 
-# mapping
-#y = y.map({'True':0, 'False':1})
-
-
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3, random_state=1, stratify=y)
-
-
 # Standardize the features
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
+X = scaler.fit_transform(X)
 
+# train test split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3, random_state=1, stratify=y)
+
+# visulize data
 import matplotlib.pyplot as plt
 colors = ['red', 'greenyellow']
 for i in range(len(colors)):
@@ -37,8 +37,8 @@ for i in range(len(colors)):
     ys = X_train[:, 1][y_train == i]
     plt.scatter(Xs, ys, c=colors[i])
 #plt.legend(iris.target_names)
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal width')
+plt.xlabel('Data')
+plt.ylabel('Distribution')
 
 # KNeighborsClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -57,19 +57,22 @@ from sklearn.metrics import classification_report,confusion_matrix
 print(confusion_matrix(y_test,y_pred))
 print(classification_report(y_test,y_pred))
 
-
 # Decision Tree Classifier
 from sklearn.tree import DecisionTreeClassifier
 #import numpy as np
 tree = DecisionTreeClassifier(criterion='gini', max_depth=4, random_state=1)
-tree.fit(X_train, y_train)
+clf = tree.fit(X_train, y_train)
 pred = tree.predict(X_test)
 
 from sklearn.metrics import classification_report,confusion_matrix
 print(confusion_matrix(y_test,y_pred))
 print(classification_report(y_test,pred))
 
-
+# visulize data
+from matplotlib import pyplot as plt
+from sklearn import tree
+fig = plt.figure(figsize=(25,20))
+_ = tree.plot_tree(clf, filled=True)
 
 print("My name is Hui-Erh Chai_Angela")
 print("My NetID is: 674939884")
